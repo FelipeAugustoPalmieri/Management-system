cb25d4ed5d48bac7437a6058469043e1347d155e
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-11-13 02:39:02 -0300

    Log logic implemented

--- app/Models/EconomicGroup.php
+++ app/Models/EconomicGroup.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class EconomicGroup extends Model
+class EconomicGroup extends Model implements Auditable
 {
-    use HasFactory;
+    use HasFactory, \OwenIt\Auditing\Auditable;
 
     protected $fillable = ['name'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuditController;
 use App\Http\Controllers\CollaboratorController;
 use App\Http\Controllers\EconomicGroupController;
 use App\Http\Controllers\FlagController;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('collaborators/report', [CollaboratorController::class, 'report'])->name('collaborators.report');
 Route::get('collaborators/export', [CollaboratorController::class, 'export'])->name('collaborators.export');
+Route::get('audits', [AuditController::class, 'showAudits'])->name('audits.index');
 
 Route::resource('economic-groups', EconomicGroupController::class);
 Route::resource('flags', FlagController::class);
